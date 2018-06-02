7e00b168328f486d8cfa4510eaddbfa13d54b281
Mark Noonan <mark@Marks-MacBook-Air.local>
2018-06-02 17:01:16 -0400

    add basic routes and schedule accordion

---
+++ src/views/Schedule.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout column align-center ma-4>
+    <h1>
+      Schedule
+    </h1>
+    <ScheduleAccordion />
+  </v-layout>
+</template>
+
+<script>
+import ScheduleAccordion from "../components/ScheduleAccordion";
+export default {
+  components: { ScheduleAccordion }
+};
+</script>
+
+<style scoped>
+</style>
