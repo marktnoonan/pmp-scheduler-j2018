cf31e85a98efaa00653031cec4b7aaf28bab3ab6
Mark Noonan <mark@Marks-MacBook-Air.local>
2018-06-02 18:36:38 -0400

    add date pickers for start and end of schedule

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-dialog
+    ref="dialog"
+    v-model="modal"
+    :return-value.sync="date"
+    persistent
+    lazy
+    full-width
+    width="290px"
+  >
+    <v-text-field
+      slot="activator"
+      v-model="date"
+      :label="label"
+      prepend-icon="event"
+      readonly
+    />
+    <v-date-picker v-model="date" :min="min" scrollable>
+      <v-spacer/>
+      <v-btn flat color="primary" @click="modal = false">Cancel</v-btn>
+      <v-btn flat color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
+    </v-date-picker>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      required: false,
+      default: "Date"
+    },
+    min: {
+      required: false,
+      default: "2018-01-01"
+    }
+  },
+  data() {
+    return {
+      date: null,
+      modal: false
+    };
+  },
+  watch: {
+    date(val) {
+      this.$emit("update", val);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -3,14 +3,38 @@
     <h1>
       Schedule
     </h1>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 md4>
+        <DatePicker label="Schedule Start Date" @update="updateStartDate"/>  
+      </v-flex>
+      <v-flex>
+        <DatePicker :min="startDate" label="Schedule End Date" @update="updateEndDate"/>
+      </v-flex>
+    </v-layout>
     <ScheduleAccordion />
   </v-layout>
 </template>
 
 <script>
 import ScheduleAccordion from "../components/ScheduleAccordion";
+import DatePicker from "../components/DatePicker";
+
 export default {
-  components: { ScheduleAccordion }
+  components: { ScheduleAccordion, DatePicker },
+  data() {
+    return {
+      startDate: "",
+      endDate: ""
+    };
+  },
+  methods: {
+    updateStartDate(payload) {
+      this.startDate = payload;
+    },
+    updateEndDate(payload) {
+      this.endDate = payload;
+    }
+  }
 };
 </script>
 
